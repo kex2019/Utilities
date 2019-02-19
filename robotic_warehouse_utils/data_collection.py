2fc2e78be05fd6470657f4d77920bd74eb12a110
jonas@valfridsson.net <jonas@valfridsson.net>
2019-02-19 12:34:12 +0100

    remove debug<br/>all good

--- robotic_warehouse_utils/data_collection.py
+++ robotic_warehouse_utils/data_collection.py
@@ -45,26 +45,33 @@ class GymCollect(object):
                     raise Exception("Dataframe has to many columns.")
                 elif "points" in list(self.df.columns) \
                         and "timestamp" in list(self.df.columns)\
-                        and "name" in list(self.df.columns)\
                         and "mean_decision_time" in list(self.df.columns)\
-                        and "steps" in list(self.df.columns)\
-                        and "total_steps" in list(self.df.columns):
+                        and "steps" in list(self.df.columns):
                     pass
                 else:
                     raise Exception("Dataframe is missing column")
-            """ ... """
             self.output = output
             self.name = name
 
             self.last_action = time.time()
             self.action_spacing = []
 
+            self.latency_meta = pd.DataFrame()
+            self.collision_meta = pd.DataFrame()
+
             self.total_steps = 0
             self.step_cap = steps
 
     def save(self):
         if hasattr(self, "df"):
-            self.df.to_csv(self.output, index=False)
+            self.df.to_csv(
+                "{}/{}.csv".format(self.output, self.name), index=False)
+            self.latency_meta.to_csv(
+                "{}/latency_{}.csv".format(self.output, self.name),
+                index=False)
+            self.collision_meta.to_csv(
+                "{}/collision_{}.csv".format(self.output, self.name),
+                index=False)
 
     def step(self, *args, **kwargs):
         self.action_spacing.append(time.time() - self.last_action)
@@ -72,20 +79,37 @@ class GymCollect(object):
         self.total_steps += 1
 
         s, rew, e, i = self.gym.step(*args, **kwargs)
+        if self.gym.round_collisions > 0:
+            data_point = {
+                "timestamp": time.time(),
+                "steps": self.total_steps,
+                "collisions": self.gym.round_collisions
+            }
+
+            self.collision_meta = self.collision_meta.append(
+                data_point, ignore_index=True)
+
+        if self.gym.round_dropoffs:
+            for dropoff in self.gym.round_dropoffs:
+                data_point = {
+                    "timestamp": time.time(),
+                    "steps": self.total_steps,
+                    "latency": self.gym.steps - dropoff.spawn,
+                    "dropped": 1
+                }
+
+                self.latency_meta = self.latency_meta.append(
+                    data_point, ignore_index=True)
 
         if rew != 0:
             data_point = {
                 "timestamp":
                 time.time(),
-                "name":
-                self.name,
                 "points":
                 rew,
                 "mean_decision_time":
                 sum(self.action_spacing) / len(self.action_spacing),
                 "steps":
-                len(self.action_spacing),
-                "total_steps":
                 self.total_steps
             }
 
@@ -95,15 +119,19 @@ class GymCollect(object):
             print("{} steps reached, saving & stopping.".format(
                 self.total_steps))
             print("See {} for saved data".format(self.output))
-            self.save()
 
-            if self.df["total_steps"].iloc[-1] < self.total_steps / 2:
-                print(
-                    "It is very likely that the algorithm made the robots get stuck"
-                )
-                print(
-                    "{} steps was taken in total but no packages was delivered after {}".
-                    format(self.total_steps, self.df["total_steps"].iloc[-1]))
+            for package in s[1]:
+                data_point = {
+                    "timestamp": time.time(),
+                    "steps": self.total_steps,
+                    "latency": self.gym.steps - package.spawn,
+                    "dropped": 0
+                }
+
+                self.latency_meta = self.latency_meta.append(
+                    data_point, ignore_index=True)
+
+            self.save()
 
             raise EvaluationDone()
         return s, rew, e, i
