a6475b76aa0ae33e47e5b638c25200901a630592
jonas@valfridsson.net <jonas@valfridsson.net>
2019-02-26 13:08:14 +0100

    This will definitely break in 2056 (TODO)

--- robotic_warehouse_utils/data_collection.py
+++ robotic_warehouse_utils/data_collection.py
@@ -7,6 +7,13 @@ class EvaluationDone(Exception):
     pass
 
 
+def initGymCollect(gym, data, output, name, steps, collect):
+    if collect:
+        return GymCollect(gym, data, output, name, steps)
+    else:
+        return gym
+
+
 class GymCollect(object):
     def __init__(
             self,
@@ -15,52 +22,50 @@ class GymCollect(object):
             output: str = None,
             name: str = "Generic101",
             # Negative steps -> no bound
-            steps: int = 10000,
-            collect=True):
-        if collect:
-            """ Hacky inheritance. """
-            self.reset = lambda: gym.reset()
-            self.render = lambda: gym.render()
-            self.map = gym.map
-            self.in_map = lambda y, x: gym.in_map(y, x)
-            self.gym = gym
+            steps: int = 10000):
+        """ Hacky inheritance. """
+        self.reset = lambda: gym.reset()
+        self.render = lambda: gym.render()
+        self.map = gym.map
+        self.in_map = lambda y, x: gym.in_map(y, x)
+        self.gym = gym
 
-            self.PICKUP_INSTRUCTION = gym.PICKUP_INSTRUCTION
-            self.DROP_INSTRUCTION = gym.DROP_INSTRUCTION
-            self.UP_INSTRUCTION = gym.UP_INSTRUCTION
-            self.DOWN_INSTRUCTION = gym.DOWN_INSTRUCTION
-            self.LEFT_INSTRUCTION = gym.LEFT_INSTRUCTION
-            self.RIGHT_INSTRUCTION = gym.RIGHT_INSTRUCTION
-            """ Validate data source. """
-            if type(data) == pd.DataFrame:
-                self.df = data
-            elif type(data) == str:
-                self.df = pd.read_csv(data)
+        self.PICKUP_INSTRUCTION = gym.PICKUP_INSTRUCTION
+        self.DROP_INSTRUCTION = gym.DROP_INSTRUCTION
+        self.UP_INSTRUCTION = gym.UP_INSTRUCTION
+        self.DOWN_INSTRUCTION = gym.DOWN_INSTRUCTION
+        self.LEFT_INSTRUCTION = gym.LEFT_INSTRUCTION
+        self.RIGHT_INSTRUCTION = gym.RIGHT_INSTRUCTION
+        """ Validate data source. """
+        if type(data) == pd.DataFrame:
+            self.df = data
+        elif type(data) == str:
+            self.df = pd.read_csv(data)
+        else:
+            print("Unsupported data type {}: {}".format(type(data), data))
+            sys.exit(1)
+
+        if list(self.df.columns):
+            if len(self.df.columns) != 5:
+                raise Exception("Dataframe has to many columns.")
+            elif "points" in list(self.df.columns) \
+                    and "timestamp" in list(self.df.columns)\
+                    and "mean_decision_time" in list(self.df.columns)\
+                    and "steps" in list(self.df.columns):
+                pass
             else:
-                print("Unsupported data type {}: {}".format(type(data), data))
-                sys.exit(1)
+                raise Exception("Dataframe is missing column")
+        self.output = output
+        self.name = name
 
-            if list(self.df.columns):
-                if len(self.df.columns) != 5:
-                    raise Exception("Dataframe has to many columns.")
-                elif "points" in list(self.df.columns) \
-                        and "timestamp" in list(self.df.columns)\
-                        and "mean_decision_time" in list(self.df.columns)\
-                        and "steps" in list(self.df.columns):
-                    pass
-                else:
-                    raise Exception("Dataframe is missing column")
-            self.output = output
-            self.name = name
+        self.last_action = time.time()
+        self.action_spacing = []
 
-            self.last_action = time.time()
-            self.action_spacing = []
+        self.latency_meta = pd.DataFrame()
+        self.collision_meta = pd.DataFrame()
 
-            self.latency_meta = pd.DataFrame()
-            self.collision_meta = pd.DataFrame()
-
-            self.total_steps = 0
-            self.step_cap = steps
+        self.total_steps = 0
+        self.step_cap = steps
 
     def save(self):
         if hasattr(self, "df"):
